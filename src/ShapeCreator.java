82037786604374083b5aa726f8d4c2b48a7ea311
fletchla <fletchla@gmail.com>
2020-05-17 23:19:59 -0700

    added shapes

--- src/ShapeCreator.java
+++ src/ShapeCreator.java
@@ -1,8 +1,11 @@
 import processing.core.PApplet;
+import java.util.ArrayList;
+
 public class ShapeCreator extends PApplet {
 
     //you'll need a structure to hold many shapes
     Shape shape;
+    ArrayList<Shape> shapes = new ArrayList<Shape>();
 
     public void settings() {
         size(500,500);
@@ -10,14 +13,20 @@ public class ShapeCreator extends PApplet {
 
     public void setup() {
         //you'll need to create all the shapes that make up your picture
-        shape = new Circle(new Point(200.0, 200.0), 50.0);
+        shapes.add(new Circle(new Point(this.width/2 - 50, 200.0), 50.0));
+        shapes.add(new Circle(new Point(this.width/2 + 50, 200.0), 50.0));
+        shapes.add(new Rectangle(new Point(this.width/2, 300), 20.0, 20.0));
+        //shape = new Circle(new Point(200.0, 200.0), 50.0);
     }
 
     public void draw() {
         //you'll need to draw all your shapes
         //this is your ShapeCreator object which is your sketch.
         //pass shape.draw the sketch so it can draw on it
-        shape.draw(this);
+        for(Shape shape: this.shapes){
+            shape.draw(this);
+        }
+
     }
 
 }
